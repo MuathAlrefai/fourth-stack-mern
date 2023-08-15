90649fcb031de39cfcad436785833914c4fe9ef4
Muath <rigahd@gmail.com>
2023-08-16 00:51:42 +0300

    14

--- React/tabs/src/components/Tabs.js
+++ React/tabs/src/components/Tabs.js
@@ -5,7 +5,7 @@ const Tabs = (props) => {
     return(
         <div style={{width:"500px", margin:"0 auto", display:"flex", justifyContent: "space-evenly", alignContent: "center"}}>
             {props.data.map((tab, i) =>
-                <div key={i} onClick={e => {props.choose(tab)}} style={{cursor: "pointer", backgroundColor: "tomato", border: "1px solid black", color: "white"}}>
+                <div key={i} onClick={e => {props.choose(tab, e)}} style={{cursor: "pointer", backgroundColor: "tomato", border: "1px solid black", color: "white"}}>
                     {tab.name}
                 </div>)}
         </div>
